8100383ff94eec67c0a58ea970d1613000089127
seth-agba <110106983+seth-agba@users.noreply.github.com>
2023-06-12 21:34:03 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,55 @@
 import React, { Component } from 'react';
 import './index.css';
-
 export default class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      clients: [
-        { id: 1, nom: 'seth agba' },
-        { id: 2, nom: 'seth Tagba' },
-        { id: 3, nom: 'seth Lo' },
-      ],
+      clients: [{ id: 1, nom: 'agba pakam ' }],
     };
+    newClient: '';
   }
   handleDelete = (id) => {
     const clients = this.state.clients.slice();
     const index = clients.findIndex((client) => client.id === id);
     clients.splice(index, 1);
-
     this.setState({ clients: clients });
-    console.log(id);
   };
-handleSubmit =event=>{
-  
-}
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const id = new Date().getTime();
+    const nom = this.state.newClient;
 
+    const clients = this.state.clients.slice();
+    const client = { id: id, nom: nom };
+    clients.push(client);
+
+    this.setState({ clients: clients, newClient: '' });
+    console.log(event.currentTarget.value);
+
+    // const nom =
+  };
+  handleChange = (event) => {
+    const value = event.currentTarget.value;
+    this.setState({ newClient: value });
+  };
   render() {
     const { clients } = this.state;
 
     return (
       <div>
-        <h1> lol</h1>
         {clients.map((client) => (
-          <li>
-            {' '}
+          <li key="index">
             {client.nom}{' '}
             <button onClick={() => this.handleDelete(client.id)}>X</button>{' '}
           </li>
         ))}
-        <form onSubmit={() => handleSubmit}>
-          <input placeholder="Nom et prenom" />
-          <button> Envoyer </button>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Nom Prénom"
+            value={this.state.newClient}
+            onChange={this.handleChange}
+          />
+          <button> Envoyer</button>
         </form>
       </div>
     );
